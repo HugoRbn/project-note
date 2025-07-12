f6534b5a8aa0331f8bbaa24d12aaf8cc93ce7b4e
Betamil <hugoribeyron1@gmail.com>
2025-07-12 11:04:34 +0200

    Init project

---
+++ src/main/java/com/project_note/project_note/ProjectNoteApplication.java
@@ -0,0 +1,13 @@
+package com.project_note.project_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project_note/project_note/ProjectNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_note.project_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
